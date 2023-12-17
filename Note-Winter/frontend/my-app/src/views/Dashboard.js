bc2d9d6970e8e9260d03c9591fa5892e6bbbbf85
Brandon Walton <BrandonWalton101102@gmail.com>
2023-12-17 02:42:33 -0600

    Loading Screen
    - Added Loading useState

--- Note-Winter/frontend/my-app/src/views/Dashboard.js
+++ Note-Winter/frontend/my-app/src/views/Dashboard.js
@@ -16,6 +16,8 @@ function Dashboard() {
   const [header, setHeader] = useState('');
   const [headerList, setHeaderList] = useState([]);
   const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
 
   const fetchUserData = async () => {
     try {
@@ -43,21 +45,18 @@ function Dashboard() {
       // Update the state with the extracted text
       setHeaderList(extractedTextHeaderArray);
       setNoteList(extractedTextBodyArray);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching user notes:', error);
     }
   };
-  
+
 
   useEffect(() => {
     console.log("Called ");
     fetchUserData();
     fetchUserNotes();
-
   }, []);
-
-
-
 
   const handleNoteChange = (event) => {
     setNote(() => event.target.value);
@@ -115,24 +114,29 @@ function Dashboard() {
           </div>
         </div>
         <br></br>
-        {noteList.length === 0 ? (<h1>Insert a Note</h1>) : (
-          <div className="note-container">
-            {
-              noteList.map((note, i) => (
-                <div key={i} className="card bg-light mb-3">
-                  <div className="card-header">{headerList[i]}</div>
-                  <div className="card-body">
-                    <p className="card-text">
-                      <input type="checkbox" className="form-check-input" style={{ position: "relative", left: "-15px" }} />
-                      <label className="form-check-label" htmlFor="exampleCheck1">{note}</label>
-                    </p>
-                    <button onClick={() => handleDelete(i)} style={{ position: "relative", left: "-100px" }}>
-                      <img src={eraser} alt="Download" style={{ width: '25px', height: '25px' }} />
-                    </button>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          noteList.length === 0 ? (<h1>Insert a Note</h1>) : (
+            <div className="note-container">
+              {
+                noteList.map((note, i) => (
+                  <div key={i} className="card bg-light mb-3">
+                    <div className="card-header">{headerList[i]}</div>
+                    <div className="card-body">
+                      <p className="card-text">
+                        <input type="checkbox" className="form-check-input" style={{ position: "relative", left: "-15px" }} />
+                        <label className="form-check-label" htmlFor="exampleCheck1">{note}</label>
+                      </p>
+                      <button onClick={() => handleDelete(i)} style={{ position: "relative", left: "-100px" }}>
+                        <img src={eraser} alt="Download" style={{ width: '25px', height: '25px' }} />
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))}
-          </div>
+                ))}
+            </div>
+          )
+
         )}
       </div>
     </div>
